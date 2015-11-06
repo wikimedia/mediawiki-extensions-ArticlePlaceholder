f251ee83234b19fbda07dc17e763d7cf0753632c
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2015-11-06 14:08:50 +0100

    Add basic PHPUnit test setup
    Change-Id: I6db2a0890e88a613cc4c11c01413413477b8c881

---
+++ tests/phpunit/Specials/SpecialAboutTopicTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ArticlePlaceholder\Tests;
+
+use ArticlePlaceholder\Specials\SpecialAboutTopic;
+use Language;
+use MediaWikiTestCase;
+use Wikibase\Client\Store\TitleFactory;
+
+/**
+ * @covers ArticlePlaceholder\Specials\SpecialAboutTopic
+ *
+ * @group ArticlePlaceholder
+ *
+ * @licence GNU GPL v2+
+ * @author Thiemo Mättig
+ */
+class SpecialAboutTopicTest extends MediaWikiTestCase {
+
+	protected function setUp() {
+		parent::setUp();
+
+		$this->setMwGlobals( array(
+			'wgContLang' => Language::factory( 'qqx' )
+		) );
+	}
+
+	public function testExecute() {
+		$termLookupFactory = $this->getMockBuilder( 'Wikibase\Lib\Store\LanguageFallbackLabelDescriptionLookupFactory' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$instance = new SpecialAboutTopic(
+			$this->getMock( 'Wikibase\DataModel\Entity\EntityIdParser' ),
+			$termLookupFactory,
+			$this->getMock( 'Wikibase\Lib\Store\SiteLinkLookup' ),
+			$this->getMock( 'SiteStore' ),
+			new TitleFactory(),
+			'',
+			$this->getMock( 'Wikibase\DataModel\Services\Lookup\EntityLookup' )
+		);
+
+		$instance->execute( '' );
+		$output = $instance->getOutput();
+		$this->assertSame( '(articleplaceholder-abouttopic)', $output->getPageTitle() );
+
+		$html = $output->getHTML();
+		$this->assertContains( 'id="ap-abouttopic-form1"', $html );
+		$this->assertContains( 'id="ap-abouttopic-entityid"', $html );
+		$this->assertContains( '(articleplaceholder-abouttopic-intro)', $html );
+		$this->assertContains( '(articleplaceholder-abouttopic-entityid)', $html );
+		$this->assertContains( '(articleplaceholder-abouttopic-submit)', $html );
+	}
+
+}
---
+++ tests/phpunit/includes/HooksTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ArticlePlaceholder\Tests;
+
+use ArticlePlaceholder\Hooks;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * @covers ArticlePlaceholder\Hooks
+ *
+ * @group ArticlePlaceholder
+ *
+ * @licence GNU GPL v2+
+ * @author Thiemo Mättig
+ */
+class HooksTest extends PHPUnit_Framework_TestCase {
+
+	public function testRegisterScribuntoExternalLibraryPaths() {
+		$instance = new Hooks();
+		$paths = array();
+		$this->assertTrue( $instance->registerScribuntoExternalLibraryPaths( 'lua', $paths ) );
+		$this->assertArrayHasKey( 0, $paths );
+		$this->assertStringEndsWith( '/includes/Lua', $paths[0] );
+	}
+
+}
