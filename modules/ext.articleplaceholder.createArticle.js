0b293d628f589e147738e85e9ca0f71b1f60cc59
Florian <florian.schmidt.stargatewissen@gmail.com>
2016-04-01 15:55:42 +0200

    Add a better error handling to the process dialog when creating an article
    It now uses the features of the OO.ui.ProcessDialog for error reporting.
    
    Change-Id: I71e6804d2e9107a90cd58bc77e9e62da043a3471

--- modules/ext.articleplaceholder.createArticle.js
+++ modules/ext.articleplaceholder.createArticle.js
@@ -8,31 +8,40 @@
 
 	var titleInput;
 
-	function onSubmit() {
+	function onSubmit( deferred ) {
 		var titleRaw = titleInput.getValue(),
 			api = new mw.Api();
 
-		api.get( { action: 'query', titles: titleRaw } ).done( function ( data ) {
-			var query = data.query,
-				pageKeys = Object.keys( query.pages ),
-				title,
-				link;
+		if ( titleRaw.trim() === '' ) {
+			deferred.reject( new OO.ui.Error(
+				mw.msg( 'articleplaceholder-abouttopic-create-article-mandatory' )
+			) );
+		} else {
+			api.get( { action: 'query', titles: titleRaw } ).done( function ( data ) {
+				var query = data.query,
+					pageKeys,
+					title,
+					link;
 
-			if ( titleRaw !== titleInput.getValue() ) {
-				return;
-			}
+				if ( query && query.hasOwnProperty( 'pages' ) ) {
+					pageKeys = Object.keys( query.pages );
+					if ( titleRaw !== titleInput.getValue() ) {
+						return;
+					}
 
-			if ( pageKeys[ 0 ] === '-1' ) {
-				title = mw.Title.newFromUserInput( titleRaw, 0 );
-				link = '?title=' + encodeURIComponent( title.getNameText() ) + '&action=edit';
-				link = mw.util.wikiScript() + link;
-				document.location.href = link;
-			} else {
-				$( '#mw-article-placeholder-error' ).append(
-					'<p>' + mw.message( 'articleplaceholder-abouttopic-article-exists-error' ).escaped() + '</p>'
-				);
-			}
-		} );
+					if ( pageKeys[ 0 ] === '-1' ) {
+						title = mw.Title.newFromUserInput( titleRaw, 0 );
+						link = '?title=' + encodeURIComponent( title.getNameText() ) + '&action=edit';
+						link = mw.util.wikiScript() + link;
+						document.location.href = link;
+					} else {
+						deferred.reject( new OO.ui.Error(
+							mw.msg( 'articleplaceholder-abouttopic-article-exists-error' )
+						) );
+					}
+				}
+			} );
+		}
 
 		return false;
 	}
@@ -41,26 +50,17 @@
 		var dialog,
 			windowManager,
 			button,
-			submitButton,
 			dialogContent;
 
 		titleInput = new OO.ui.TextInputWidget( {
 			value: mw.config.get( 'apLabel' ),
 			label: mw.msg( 'articleplaceholder-abouttopic-create-article-label' ),
 			multiline: false,
+			required: true,
 			autosize: true
 		} );
 
 		dialogContent = titleInput.$element;
-		dialogContent.append( '<div id="mw-article-placeholder-error"></div>' );
-
-		titleInput.on( 'change', function () {
-			$( '#mw-article-placeholder-error' ).empty();
-		} );
-
-		titleInput.on( 'enter', function () {
-			submitButton.emit( 'click' );
-		} );
 
 		function CreateArticleDialog( config ) {
 			CreateArticleDialog.super.call( this, config ); // jshint:ignore
@@ -86,14 +86,15 @@
 		};
 
 		CreateArticleDialog.prototype.getBodyHeight = function () {
-			return this.content.$element.outerHeight( true );
+			return this.content.$element.outerHeight( true ) * 2;
 		};
 
 		CreateArticleDialog.prototype.getActionProcess = function ( action ) {
 			if ( action ) {
 				return new OO.ui.Process( function () {
 					var saveDeferred = $.Deferred();
-					onSubmit();
+					onSubmit( saveDeferred );
+
 					return saveDeferred.promise();
 				}, this );
 			}
@@ -102,6 +103,10 @@
 
 		dialog = new CreateArticleDialog( {
 			size: 'medium'
+		} );
+
+		titleInput.on( 'enter', function () {
+			dialog.executeAction( 'save' );
 		} );
 
 		windowManager = new OO.ui.WindowManager();
