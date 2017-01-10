7afad2423595567aedace5455481c0ac1602b1ef
Marius Hoch <hoo@online.de>
2017-01-09 16:53:33 -0800

    Change key of wgExtensionMessagesFiles entry
    Needs to be distinct from the wgMessagesDirs entry, per the documentation
    in DefaultSettings.php.
    Also made consistent with the extension.json.
    
    Change-Id: I919d6e4871ff5195bd78524ab0c968d2609e7031

--- ArticlePlaceholder.php
+++ ArticlePlaceholder.php
@@ -15,7 +15,7 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'ArticlePlaceholder', __DIR__ . '/extension.json' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['ArticlePlaceholder'] = __DIR__ . '/i18n';
-	$wgExtensionMessagesFiles['ArticlePlaceholder'] = __DIR__ . '/ArticlePlaceholder.alias.php';
+	$wgExtensionMessagesFiles['ArticlePlaceholderAliases'] = __DIR__ . '/ArticlePlaceholder.alias.php';
 	/* wfWarn(
 		'Deprecated PHP entry point used for ArticlePlaceholder extension. ' .
 		'Please use wfLoadExtension instead, ' .
