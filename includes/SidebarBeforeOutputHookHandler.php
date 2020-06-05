501d863046cef9e9e95cc0496f4d66bab0fd6bdb
Ammar Abdulhamid <ammarpad@yahoo.com>
2020-06-05 11:24:24 +0100

    Use SidebarBeforeOutput hook to add items to sidebar toolbox
    BaseTemplateToolbox hook is being deprecated. Toolbox item is now
    added with SidebarBeforeOutput hook
    
    Bug: T254565
    Change-Id: I05dae74b5071ffc668c3569566239052575edfb9

---
+++ includes/SidebarBeforeOutputHookHandler.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace ArticlePlaceholder;
+
+use Exception;
+use MediaWiki\MediaWikiServices;
+use Skin;
+use Wikibase\Client\RepoLinker;
+use Wikibase\Client\WikibaseClient;
+use Wikibase\DataModel\Entity\EntityIdParser;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\DataModel\Services\Lookup\EntityLookup;
+
+/**
+ * Add Wikibase item link in toolbox for placeholders: Handler for the "SidebarBeforeOutput" hook.
+ *
+ * @license GPL-2.0-or-later
+ * @author Marius Hoch < hoo@online.de >
+ */
+class SidebarBeforeOutputHookHandler {
+
+	/**
+	 * @var EntityIdParser
+	 */
+	private $entityIdParser;
+
+	/**
+	 * @var RepoLinker
+	 */
+	private $repoLinker;
+
+	/**
+	 * @var EntityLookup
+	 */
+	private $entityLookup;
+
+	/**
+	 * @return self
+	 */
+	private static function newFromGlobalState() {
+		$wikibaseClient = WikibaseClient::getDefaultInstance();
+
+		return new self(
+			$wikibaseClient->getEntityIdParser(),
+			$wikibaseClient->newRepoLinker(),
+			$wikibaseClient->getStore()->getEntityLookup()
+		);
+	}
+
+	public function __construct(
+		EntityIdParser $entityIdParser,
+		RepoLinker $repoLinker,
+		EntityLookup $entityLookup
+	) {
+		$this->entityIdParser = $entityIdParser;
+		$this->repoLinker = $repoLinker;
+		$this->entityLookup = $entityLookup;
+	}
+
+	/**
+	 * @param Skin $skin
+	 * @param array[] &$sidebar
+	 *
+	 * @return bool|void
+	 */
+	public static function onSidebarBeforeOutput( Skin $skin, &$sidebar ) {
+		$self = self::newFromGlobalState();
+		$sidebarLink = $self->buildSidebarLink( $skin );
+
+		if ( !$sidebarLink ) {
+			return false;
+		}
+
+		// Append link
+		$sidebar['TOOLBOX']['wikibase'] = $sidebarLink;
+	}
+
+	/**
+	 * Do checks. Build link array, if possible.
+	 *
+	 * @param Skin $skin
+	 *
+	 * @return bool|string[] Array of link elements or False if link cannot be craated
+	 */
+	public function buildSidebarLink( Skin $skin ) {
+		// Return early (for performance reasons) in case we're not on
+		// Special:AboutTopic (even before calling newFromGlobalState)
+		$title = $skin->getTitle();
+
+		if ( $title->getNamespace() !== NS_SPECIAL ) {
+			return false;
+		}
+
+		$factory = MediaWikiServices::getInstance()->getSpecialPageFactory();
+		$canonicalSpecialPageName = $factory->resolveAlias( $title->getText() )[0];
+
+		if ( $canonicalSpecialPageName !== 'AboutTopic' ) {
+			return false;
+		}
+
+		$itemId = $this->getItemId( $skin );
+
+		if ( $itemId === null || !$this->entityLookup->hasEntity( $itemId ) ) {
+			return false;
+		}
+		// Duplicated from Wikibase\ClientHooks::buildWikidataItemLink
+		return [
+			'id' => 't-wikibase',
+			'text' => $skin->msg( 'wikibase-dataitem' )->text(),
+			'href' => $this->repoLinker->getEntityUrl( $itemId )
+		];
+	}
+
+	/**
+	 * @param Skin $skin
+	 *
+	 * @return ItemId|null
+	 */
+	private function getItemId( Skin $skin ): ?ItemId {
+		$title = $skin->getTitle();
+		$request = $skin->getRequest();
+
+		$factory = MediaWikiServices::getInstance()->getSpecialPageFactory();
+		$idSerialization = $request->getText(
+			'entityid',
+			$factory->resolveAlias( $title->getText() )[1]
+		);
+
+		if ( !$idSerialization ) {
+			return null;
+		}
+
+		try {
+			return $this->entityIdParser->parse( $idSerialization );
+		} catch ( Exception $ex ) {
+			// Ignore
+		}
+
+		return null;
+	}
+
+}
---
+++ tests/phpunit/includes/SidebarBeforeOutputHookHandlerTest.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace ArticlePlaceholder\Tests;
+
+use ArticlePlaceholder\SidebarBeforeOutputHookHandler;
+use MediaWikiTestCase;
+use ReflectionMethod;
+use Skin;
+use Title;
+use WebRequest;
+use Wikibase\Client\Tests\Mocks\MockClientStore;
+use Wikibase\Client\WikibaseClient;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\ItemId;
+
+/**
+ * @group ArticlePlaceholder
+ *
+ * @covers ArticlePlaceholder\SidebarBeforeOutputHookHandler
+ *
+ * @license GPL-2.0-or-later
+ * @author Marius Hoch < hoo@online.de >
+ */
+class SidebarBeforeOutputHookHandlerTest extends MediaWikiTestCase {
+
+	public function setUp() : void {
+		parent::setUp();
+
+		$this->setUserLang( 'qqx' );
+	}
+
+	protected function tearDown() : void {
+		WikibaseClient::getDefaultInstance( 'reset' );
+
+		parent::tearDown();
+	}
+
+	protected function testNewFromGlobalState() {
+		$reflectionMethod = new ReflectionMethod(
+			SidebarBeforeOutputHookHandler::class,
+			'newFromGlobalState'
+		);
+		$reflectionMethod->setAccessible( true );
+		$handler = $reflectionMethod->invoke( null );
+
+		$this->assertInstanceOf( SidebarBeforeOutputHookHandler::class, $handler );
+	}
+
+	public function testBuildSidebarLink_wrongNamespace() {
+		$title = $this->createMock( Title::class );
+		$title->expects( $this->once() )
+			->method( 'getNamespace' )
+			->will( $this->returnValue( 2 ) );
+
+		// We abort early, before Title::getText is ever called
+		$title->expects( $this->never() )
+			->method( 'getText' );
+
+		$skin = $this->getSkin( $title );
+
+		$result = $this->getHookHandler()->buildSidebarLink( $skin );
+
+		$this->assertFalse( $result );
+	}
+
+	public function testBuildSidebarLink_wrongSpecialPageName() {
+		$title = $this->createMock( Title::class );
+		$title->expects( $this->once() )
+			->method( 'getNamespace' )
+			->will( $this->returnValue( NS_SPECIAL ) );
+
+		// Title::getText is only called once, because we abort after checking the name
+		$title->expects( $this->once() )
+			->method( 'getText' )
+			->will( $this->returnValue( 'Preferences' ) );
+
+		$skin = $this->getSkin( $title );
+
+		$result = $this->getHookHandler()->buildSidebarLink( $skin );
+
+		$this->assertFalse( $result );
+	}
+
+	public function testBuildSidebarLink_invalidItemId() {
+		$title = $this->getAboutTopicTitle( 'not-an-item-id' );
+
+		$skin = $this->getSkin( $title );
+
+		$result = $this->getHookHandler()->buildSidebarLink( $skin );
+
+		$this->assertFalse( $result );
+	}
+
+	public function testBuildSidebarLink_unknownItemId() {
+		$title = $this->getAboutTopicTitle( 'Q42' );
+
+		$skin = $this->getSkin( $title );
+
+		$result = $this->getHookHandler()->buildSidebarLink( $skin );
+
+		$this->assertFalse( $result );
+	}
+
+	public function testBuildSidebarLink_subPage() {
+		$title = $this->getAboutTopicTitle( 'Q2013' );
+
+		$skin = $this->getSkin( $title );
+
+		$result = $this->getHookHandler()->buildSidebarLink( $skin );
+
+		$this->assertLinkArray( $result );
+	}
+
+	public function testBuildSidebarLink_entityidParam() {
+		$title = $this->getAboutTopicTitle();
+
+		$skin = $this->getSkin( $title, 'Q2013' );
+
+		$result = $this->getHookHandler()->buildSidebarLink( $skin );
+
+		$this->assertLinkArray( $result );
+	}
+
+	public function testBuildSidebarLink_entityidParamOverridesSubPage() {
+		// This matches the special page's behaviour.
+		$title = $this->getAboutTopicTitle( 'Q123' );
+
+		$skin = $this->getSkin( $title, 'Q2013' );
+
+		$result = $this->getHookHandler()->buildSidebarLink( $skin );
+
+		$this->assertLinkArray( $result );
+	}
+
+	private function assertLinkArray( $result ) {
+		$itemId = new ItemId( 'Q2013' );
+		$href = WikibaseClient::getDefaultInstance()->newRepoLinker()->getEntityUrl( $itemId );
+
+		$link = [
+			'id' => 't-wikibase',
+			'text' => '(wikibase-dataitem)',
+			'href' => $href
+		];
+
+		$this->assertSame( $link, $result );
+	}
+
+	/**
+	 * @param string|null $subPage
+	 *
+	 * @return Title
+	 */
+	private function getAboutTopicTitle( $subPage = null ) {
+		$titleText = 'AboutTopic';
+
+		if ( $subPage ) {
+			$titleText .= '/' . $subPage;
+		}
+
+		$title = $this->createMock( Title::class );
+		$title->expects( $this->once() )
+			->method( 'getNamespace' )
+			->will( $this->returnValue( NS_SPECIAL ) );
+
+		$title->expects( $this->exactly( 2 ) )
+			->method( 'getText' )
+			->will( $this->returnValue( $titleText ) );
+
+		return $title;
+	}
+
+	/**
+	 * @param Title $title
+	 * @param string|null $itemIdParam
+	 *
+	 * @return Skin
+	 */
+	private function getSkin( Title $title, $itemIdParam = null ) {
+		$request = $this->createMock( WebRequest::class );
+		$request->expects( $this->any() )
+			->method( 'getText' )
+			->will( $this->returnCallback( function ( $name, $default ) use ( $itemIdParam ) {
+				$this->assertSame( 'entityid', $name );
+
+				return $itemIdParam ?: $default;
+			} ) );
+
+		$skin = $this->getMockBuilder( Skin::class )
+			->setMethods( [ 'getTitle', 'getRequest' ] )
+			->getMockForAbstractClass();
+
+		$skin->expects( $this->any() )
+			->method( 'getTitle' )
+			->will( $this->returnValue( $title ) );
+
+		$skin->expects( $this->any() )
+			->method( 'getRequest' )
+			->will( $this->returnValue( $request ) );
+
+		return $skin;
+	}
+
+	private function getHookHandler() {
+		$clientStore = new MockClientStore();
+		$wbClient = WikibaseClient::getDefaultInstance();
+		$wbClient->overrideStore( $clientStore );
+
+		$mockRepository = $clientStore->getEntityRevisionLookup();
+		$item = new Item( new ItemId( 'Q2013' ) );
+
+		$mockRepository->putEntity( $item );
+
+		return new SidebarBeforeOutputHookHandler(
+			$wbClient->getEntityIdParser(),
+			$wbClient->newRepoLinker(),
+			$wbClient->getStore()->getEntityLookup()
+		);
+	}
+
+}
