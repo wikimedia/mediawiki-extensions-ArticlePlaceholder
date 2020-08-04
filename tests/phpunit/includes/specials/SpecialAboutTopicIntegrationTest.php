d275044926ff86b5b96c64efb6c272932981f024
Tobias Andersson <tobias.andersson@wikimedia.de>
2020-08-04 17:56:02 +0200

    Add integration test for buildProjectLinkSidebarFromItemId
    Bug: T258184
    Co-Authored-by: gtzatchkova <guergana.tzatchkova@wikimedia.de>
    Co-Authored-by: Michael Große <michael.grosse@wikimedia.de>
    Change-Id: I3d6143aadb554f36efc961123bc576966fde3672

---
+++ tests/phpunit/includes/specials/SpecialAboutTopicIntegrationTest.php
@@ -0,0 +1,151 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ArticlePlaceholder\Tests\Specials;
+
+use ArticlePlaceholder\AboutTopicRenderer;
+use ArticlePlaceholder\Specials\SpecialAboutTopic;
+use HashSiteStore;
+use MediaWiki\MediaWikiServices;
+use Site;
+use SiteLookup;
+use SpecialPageTestBase;
+use TitleFactory;
+use Wikibase\Client\Hooks\OtherProjectsSidebarGeneratorFactory;
+use Wikibase\Client\Tests\Mocks\MockClientStore;
+use Wikibase\Client\WikibaseClient;
+use Wikibase\DataModel\Services\Lookup\InMemoryEntityLookup;
+use Wikibase\DataModel\SiteLink;
+use Wikibase\DataModel\SiteLinkList;
+use Wikibase\Lib\SettingsArray;
+use Wikibase\Repo\Tests\NewItem;
+
+/**
+ * @covers ArticlePlaceholder\Specials\SpecialAboutTopic
+ *
+ * @group ArticlePlaceholder
+ * @group Database
+ *
+ * @license GPL-2.0-or-later
+ */
+class SpecialAboutTopicIntegrationTest extends SpecialPageTestBase {
+
+	/**
+	 * @var OutputPage
+	 */
+	private $page;
+
+	/**
+	 * @var WikibaseClient
+	 */
+	private $wikibaseClient;
+
+	/**
+	 * @var MockClientStore
+	 */
+	private $store;
+
+	/**
+	 * @var InMemoryEntityLookup
+	 */
+	private $inMemoryLookup;
+
+	protected function setUp() : void {
+		parent::setUp();
+
+		$this->insertPage( 'Template:AboutTopic', '(aboutTopic: {{{1}}})' );
+		$this->wikibaseClient = WikibaseClient::getDefaultInstance( 'reset' );
+
+		$this->store = new MockClientStore( 'sv' );
+
+		$testItem = NewItem::withId( 'Q1' )->build();
+		$testItem->setSiteLinkList( new SiteLinkList( [ new SiteLink( 'non-default', 'Wikipedia' ) ] ) );
+
+		$this->inMemoryLookup = new InMemoryEntityLookup( $testItem );
+	}
+
+	protected function tearDown(): void {
+		WikibaseClient::getDefaultInstance( 'reset' );
+		parent::tearDown();
+	}
+
+	private function newSettings() {
+		$defaults = [
+			'siteGlobalID' => 'local-non-default',
+			'otherProjectsLinks' => [ 'non-default' ],
+		];
+
+		return new SettingsArray( $defaults );
+	}
+
+	/**
+	 * @return SiteLookup
+	 */
+	private function getSiteLookupMock() {
+		$sites = [];
+
+		$site = new Site();
+		$site->setGlobalId( 'non-default' );
+		$site->setGroup( 'wikipedia' );
+		$site->setLanguageCode( 'sv' );
+		$site->setLinkPath( 'http://www.something-cool.se' );
+		$sites[] = $site;
+
+		return new HashSiteStore( $sites );
+	}
+
+	protected function newSpecialPage() {
+		$articlePlaceholderSearchEngineIndexed = MediaWikiServices::getInstance()->getMainConfig()->get(
+			'ArticlePlaceholderSearchEngineIndexed'
+		);
+
+		$siteLookup = $this->getSiteLookupMock();
+		$titleFactory = new TitleFactory();
+		$settings = $this->newSettings();
+
+		$factory = new OtherProjectsSidebarGeneratorFactory(
+			$settings,
+			$this->store->getSiteLinkLookup(),
+			$siteLookup,
+			$this->inMemoryLookup,
+			$this->wikibaseClient->getSidebarLinkBadgeDisplay(),
+			MediaWikiServices::getInstance()->getHookContainer(),
+			$this->wikibaseClient->getLogger()
+		);
+
+		$this->page = new SpecialAboutTopic(
+			new AboutTopicRenderer(
+				$this->wikibaseClient->getLanguageFallbackLabelDescriptionLookupFactory(),
+				$this->store->getSiteLinkLookup(),
+				$siteLookup,
+				$this->wikibaseClient->getLangLinkSiteGroup(),
+				$titleFactory,
+				$factory,
+				MediaWikiServices::getInstance()->getPermissionManager()
+			),
+			$this->wikibaseClient->getEntityIdParser(),
+			$this->store->getSiteLinkLookup(),
+			$titleFactory,
+			$settings->getSetting( 'siteGlobalID' ),
+			$this->inMemoryLookup,
+			$articlePlaceholderSearchEngineIndexed
+		);
+
+		return $this->page;
+	}
+
+	public function testExecution() {
+		list( $specialPageResult, ) = $this->executeSpecialPage( 'Q1' );
+		$expected = '<p>(aboutTopic: Q1)' . "\n</p>";
+
+		$output = $this->page->getOutput();
+		$sidebar = $output->getProperty( 'wikibase-otherprojects-sidebar' );
+
+		$this->assertSame( $expected, $specialPageResult );
+		$this->assertSame( 'http://www.something-cool.se', $sidebar[0]['href'] );
+		$this->assertSame( 'wb-otherproject-link wb-otherproject-wikipedia', $sidebar[0]['class'] );
+		$this->assertSame( 'sv', $sidebar[0]['hreflang'] );
+		$this->assertSame( 'wikibase-otherprojects-wikipedia', $sidebar[0]['msg'] );
+	}
+}
